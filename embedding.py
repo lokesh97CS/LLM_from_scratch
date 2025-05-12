90d937b82fb4704aa64c6037e46b0a00dad2b761
Lokesh Yadav <lokeshyadavmandapalli@gmail.com>
2025-05-12 22:23:36 +0530

    added parser

--- embedding.py
+++ embedding.py
@@ -1,6 +1,7 @@
 import urllib.request
 import os
 import re
+import argparse
 
 class Simple_Tokenizer:
    def __init__(self, vocab):
@@ -27,14 +28,18 @@ def preprocess(file_name , is_vocab):
        return pre_processed
         
 if __name__== "__main__":
+
     ## make a parser
     ## should take a file for building vocabulary
     ## should a take as input
+    parser=argparse.ArgumentParser(description='taking vocan and input files for embeddings')
+    parser.add_argument('-v', '--vocab', required =False, default= 'vocab.txt', type=str, help='vocab file')
+    parser.add_argument('-i', '--input', required =False, type=str,default= 'sample_input.txt', help='input file')
+    args = parser.parse_args()
 
-
-    vocab=preprocess("vocab.txt" , True)
+    vocab=preprocess(args.vocab , True)
     vocab.append('<unk_token>')
-    tokens=preprocess("sample_input.txt" , False)
+    tokens=preprocess(args.input, False)
 
     Tokenizer_object = Simple_Tokenizer(vocab)
     Tokenizer_object.encode(tokens)
